3999381f05172760a8bb9ea92bbbf17d7d93dfac
WillPy <158475553+WillPy45@users.noreply.github.com>
2024-05-14 18:38:40 -0400

    Add files via upload

--- Cash.c
+++ Cash.c
@@ -50,12 +50,13 @@ int get_integer(char* imprimir){
     //Si entrada es correcta funcion scanf retorna 1, caso incorrecto, retorna 0;
     do
     {
-        printf("%s", imprimir); //Imprime segun la entrada get_init("TEXTO QUE SE IMPRIME")
+        printf("%s", imprimir); //Imprime segun la entrada get_integer("TEXTO QUE SE IMPRIME")
         Verif = scanf("%i", &num);
-        //Si scanf no falló, limpiar bufer
-        if (Verif == 0)
+        //Si scanf falló, limpiar bufer
+        if (Verif == 0 || getchar() != '\n')
         {
-            while (getchar() != '\n');
+            while (getchar() != '\n'); //LIMPIEZA DE BUFER
+            Verif = 0; //Declarar invalido por encontrar un caracter prohibido
         }
     } while (Verif == 0);
    
