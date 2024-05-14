7c545473084338f92197046ac78eb48d32e5b193
WillPy45 <158475553+WillPy45@users.noreply.github.com>
2024-05-14 14:52:28 -0400

    Add files via upload

--- Cash.c
+++ Cash.c
@@ -1,6 +1,68 @@
 #include <stdio.h>
-
 /* Dado un monto en centavos, determina la cantidad mínima de monedas que debes dar como cambio */
+
+//Prototipos
+int get_interger(char* imprimir); //Entrada de solo enteros.
+int CambioMinimo(int Centavos, int *dir1, int *dir2, int *dir3, int *dir4); //
+
+
+//Programa
+int main(void)
+{
+    // Variables
+    int C25 = 0, C10 = 0, C5 = 0, C1 = 0; // Monedas
+    // Direcciones de memoria de las variables de monedas
+    int *dirC25 = &C25;
+    int *dirC10 = &C10;
+    int *dirC5 = &C5;
+    int *dirC1 = &C1;
+
+    int Centavos = 0;
+   
+    // [Entrada]
+    do
+    {
+    
+        printf("Rango de cambio debido: números enteros positivos (no 0)\n");
+        Centavos = get_interger("Cambio debido en centavos: ");
+            
+        if (Centavos <= 0)
+        {
+            printf("El cambio debido está fuera del rango. Introduce un número entero mayor que 0.\n");
+        }
+        
+    }while (Centavos <= 0);
+
+    // [Proceso] Determinar la cantidad mínima de monedas
+    int MonedasNecesarias = CambioMinimo(Centavos, dirC25, dirC10, dirC5, dirC1);
+
+    // [Salida] Monedas necesarias y distribución de denominaciones de monedas
+    printf("Monedas necesarias: %i\nDistribución: (%d x 25c | %d x 10c | %d x 5c | %d x 1c)\n", MonedasNecesarias, C25, C10, C5, C1);
+    return 0;
+}
+
+
+//Funciones
+int get_interger(char* imprimir){
+    //Esta funcion lee hasta la ultima entrada valida.
+    int num = 0;
+    int Verif = 0;
+    //Si entrada es correcta funcion scanf retorna 1, caso incorrecto, retorna 0;
+    do
+    {
+        printf("%s", imprimir); //Imprime segun la entrada get_init("TEXTO QUE SE IMPRIME")
+        Verif = scanf("%i", &num);
+        //Si scanf no falló, limpiar bufer
+        if (Verif == 0)
+        {
+            while (getchar() != '\n');
+        }
+    } while (Verif == 0);
+   
+    return num;
+    
+}
+
 int CambioMinimo(int Centavos, int *dir1, int *dir2, int *dir3, int *dir4)
 {
     int Cents = Centavos;
@@ -35,36 +97,4 @@ int CambioMinimo(int Centavos, int *dir1, int *dir2, int *dir3, int *dir4)
     } while (Cents > 0);
 
     return Monedas;
-}
-
-int main(void)
-{
-    // Variables
-    int C25 = 0, C10 = 0, C5 = 0, C1 = 0; // Monedas
-    // Direcciones de memoria de las variables de monedas
-    int *dirC25 = &C25;
-    int *dirC10 = &C10;
-    int *dirC5 = &C5;
-    int *dirC1 = &C1;
-
-    int Centavos = 0;
-
-    // [Entrada]
-    do
-    {
-        printf("Rango de cambio debido: números enteros positivos (no 0)\n");
-        printf("Cambio debido en centavos: ");
-        scanf("%d", &Centavos);
-        if (Centavos <= 0)
-        {
-            printf("El cambio debido está fuera del rango. Introduce un número entero mayor que 0.\n");
-        }
-    } while (Centavos <= 0);
-
-    // [Proceso] Determinar la cantidad mínima de monedas
-    int MonedasNecesarias = CambioMinimo(Centavos, dirC25, dirC10, dirC5, dirC1);
-
-    // [Salida] Monedas necesarias y distribución de denominaciones de monedas
-    printf("Monedas necesarias: %i\nDistribución: (%d x 25c | %d x 10c | %d x 5c | %d x 1c)\n", MonedasNecesarias, C25, C10, C5, C1);
-    return 0;
 }
