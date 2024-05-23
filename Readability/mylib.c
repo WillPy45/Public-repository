adff2fb2a2715bead89f859600b319fe73d0bd73
WillPy <158475553+WillPy45@users.noreply.github.com>
2024-05-23 05:38:15 -1200

    Update mylib.c

--- Readability/mylib.c
+++ Readability/mylib.c
@@ -1,4 +1,6 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "mylib.h"
 
 int get_integer(char* imprimir){
@@ -18,5 +20,40 @@ int get_integer(char* imprimir){
         }
     } while (Verif == 0);
     return num;
+}
+
+void get_stringss(char *imprimir, char STRING[], int sizeSTR)
+{// args: STRING A IMPRIMIR | STRING A MANIPULAR | TAMAÑO DEL ARRAY
+    char temp = 'F';
+    int desb = 1;
+    do
+    {
+        temp = 'F';
+        desb = 1;
+        printf("%s", imprimir);
+        fgets(STRING, sizeSTR, stdin);
+        for (int j = 0; j <= sizeSTR - 2; j++)
+        {
+            if (STRING[j] == '\n')
+            {
+                desb = 0;
+            }
+        }
+        if (desb == 1)
+        { // En caso de deborde, limpiar bufer
+            printf("CUIDADO! EL BUFER SE HA DESBORDADO, SE PROCEDERÁ A LA LIMPIEZA DE LOS "
+                   "CARACTERES EXCEDENTES.\n MAX_CARACTERES: %i - 1 (NULL CHARACTER). \n --- RECUERDA QUE ENTER TAMBIEN ES UN CARACTER---\n",
+                   sizeSTR);
+            printf("\nCAUTION! THE BUFFER HAS OVERFLOWED, THEY WILL BE CLEANED"
+            "SURPLUS CHARACTERS.\n MAX_CHARACTERS: %i - 1 (NULL CHARACTER). \n --- REMEMBER THAT ENTER IS ALSO A CHARACTER---\n", sizeSTR);
+            printf("\nCaracteres excedidos / Exceeded character: ");
+            do
+            {
+                temp = getchar();
+                printf("%c (%i) | ", temp, temp);
+            }while (temp != '\0' && temp != '\n');
+            printf("\n");
+        }
+    }while (desb == 1);
 }
 
