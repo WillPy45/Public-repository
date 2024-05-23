440e40d2933280c718a90a13b28810a3e7602fff
WillPy <158475553+WillPy45@users.noreply.github.com>
2024-05-23 04:31:22 -1200

    Update mylib.c
    new function

--- MyLib/mylib.c
+++ MyLib/mylib.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "mylib.h"
-
 int get_integer(char* imprimir){
     
     int num = 0;
@@ -19,4 +20,37 @@ int get_integer(char* imprimir){
     } while (Verif == 0);
     return num;
 }
+
+
+void get_stringss(char *imprimir, char STRING[], int sizeSTR)
+{// args: STRING A IMPRIMIR | STRING A MANIPULAR | TAMAÑO DEL ARRAY
+    char temp = 'F';
+    int desb = 1;
+    do
+    {
+        temp = 'F';
+        desb = 1;
+        printf("%s", imprimir);
+        fgets(STRING, sizeSTR, stdin);
+        for (int j = 0; j <= sizeSTR - 2; j++)
+        {
+            if (STRING[j] == '\n')
+            {
+                desb = 0;
+            }
+        }
+        if (desb == 1)
+        { // En caso de deborde, limpiar bufer
+            printf("CUIDADO! EL BUFER SE HA DESBORDADO, SE PROCEDERÁ A LA LIMPIEZA DE LOS "
+                   "CARACTERES EXCEDENTES.\n MAX_CARACTERES: %i - 1 (NULL CHARACTER). \n --- RECUERDA QUE ENTER TAMBIEN ES UN CARACTER---\n",
+                   sizeSTR);
+            do
+            {
+                temp = getchar();
+                printf("\nBasura: %c (%i)\n", temp, temp);
+            }while (temp != '\0' && temp != '\n');
+        }
+    }while (desb == 1);
+}
+
 
