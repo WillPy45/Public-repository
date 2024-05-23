a57d868749733fbe73db3d2515f72650a96b431f
WillPy <158475553+WillPy45@users.noreply.github.com>
2024-05-23 05:42:56 -1200

    Update mylib.h

--- Readability/mylib.h
+++ Readability/mylib.h
@@ -1,4 +1,8 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "mylib.h"
+
 //Prototipos
 int get_integer(char* imprimir);
 void get_stringss(char *imprimir, char STRING[], int sizeSTR);
